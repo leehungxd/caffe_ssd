ffc9492967a41a0cf4feff616d83f2bc915e8fcc
leehungxd <leehungxd@163.com>
2019-03-28 16:24:57 +0800

    caffe-ssd original

---
+++ data/bdd100k/create_data.sh
@@ -0,0 +1,25 @@
+cur_dir=$(cd $( dirname ${BASH_SOURCE[0]} ) && pwd )
+root_dir=$cur_dir/../..
+
+cd $root_dir
+
+redo=1
+data_root_dir="/caffe/train_data/bdd100k"
+dataset_name="bdd100k"
+mapfile="/caffe/train_data/bdd100k/labelmap_bdd100k.prototxt"
+anno_type="detection"
+db="lmdb"
+min_dim=0
+max_dim=0
+width=0
+height=0
+
+extra_cmd="--encode-type=jpg --encoded"
+if [ $redo ]
+then
+  extra_cmd="$extra_cmd --redo"
+fi
+for subset in val train
+do
+  python $root_dir/scripts/create_annoset.py --anno-type=$anno_type --label-map-file=$mapfile --min-dim=$min_dim --max-dim=$max_dim --resize-width=$width --resize-height=$height --check-label $extra_cmd $data_root_dir $root_dir/data/$dataset_name/$subset.txt $data_root_dir/$db/$dataset_name"_"$subset"_"$db examples/$dataset_name
+done
